7b91acc47a8d48a7c35e9d794c011e39d9c81878
bstoler@google.com <bstoler@google.com@1a334de8-7455-0410-9b2e-eb0121b2e18b>
2009-02-12 18:46:10 +0000

    Disable NonPublicTest since it fails in Web mode. Was still useful while developing.

--- test/com/google/gwt/inject/client/nonpublic/NonPublicTest.java
+++ test/com/google/gwt/inject/client/nonpublic/NonPublicTest.java
@@ -15,22 +15,28 @@
  */
 package com.google.gwt.inject.client.nonpublic;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.junit.client.GWTTestCase;
 
 /**
  * Tests for issue #1. We don't actually support injecting non-public types,
  * so this Ginjector creation is expected to fail. This test does illustrate
  * that we give good error messages in this case. (They are not asserted but
- * you can see them in the output.)
+ * you can see them in the output.) Unfortunately, this intentional creation of
+ * a class with failures in the generator only works in hosted mode, so
+ * the actual test contents are commented out. You can uncomment them to see
+ * what the error messages are like.
  */
 public class NonPublicTest extends GWTTestCase {
   public void testNonPublic() throws Exception {
+    // Do nothing. :(
+    
+    /*
     try {
       NonPublicGinjector ginjector = GWT.create(NonPublicGinjector.class);
     } catch (RuntimeException e) {
       // Expected it to fail. Nothing much more specific to assert.
     }
+    */
 
     // This is what we would test if we actually supported injecting non-public classes
     /*
