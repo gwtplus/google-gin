d10c1f2bd2a328e127714c8d028758fe59b49d6e
aragos <aragos@1a334de8-7455-0410-9b2e-eb0121b2e18b>
2009-12-21 16:29:57 +0000

    Added new sample application (issue #67)

---
+++ samples/simple/src/com/google/gwt/inject/example/simple/client/SimpleWidget.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.gwt.inject.example.simple.client;
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.inject.Inject;
+
+/**
+ * A widget that displays a message or an error and allows the user to show more
+ * messages by providing "Show Error" and "Show Message" buttons.
+ */
+public class SimpleWidget extends Composite {
+
+  private final SimpleMessages messages;
+  private final Label text;
+  private final SimpleService service;
+
+  /**
+   * Constructs a new simple widget.
+   *
+   * @param messages a message interface providing message and error templates
+   * @param service a service that returns fresh messages and errors
+   * @param constants constants to label the buttons
+   */
+  @Inject
+  public SimpleWidget(SimpleMessages messages, SimpleService service, SimpleConstants constants) {
+    this.messages = messages;
+    this.service = service;
+
+    text = new Label();
+    text.addStyleName("message");
+
+    Button showMessage = new Button(constants.showMessage(), new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        showMessage();
+      }
+    });
+
+    Button showError = new Button(constants.showError(), new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        showError();
+      }
+    });
+
+    HorizontalPanel buttons = new HorizontalPanel();
+    buttons.add(showMessage);
+    buttons.add(showError);
+
+    VerticalPanel root = new VerticalPanel();
+    root.add(text);
+    root.add(buttons);
+    root.addStyleName("simple");
+
+    initWidget(root);
+  }
+
+  /**
+   * Shows a message on the screen.
+   */
+  public void showMessage() {
+    text.setText(messages.messageTemplate(service.getRandomMessage()));
+    text.removeStyleName("error");
+  }
+
+  /**
+   * Shows an error on the screen.
+   */
+  public void showError() {
+    text.setText(messages.errorTemplate(service.getRandomError()));
+    text.addStyleName("error");
+  }
+}
