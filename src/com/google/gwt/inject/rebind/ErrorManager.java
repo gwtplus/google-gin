43bbbccc7d1ef9d5fc51309cfe01bb7b945747ca
aragos <aragos@1a334de8-7455-0410-9b2e-eb0121b2e18b>
2011-01-20 03:14:59 +0000

    Dedicated error handling in BindingProcessor (by bchambers), part 2

---
+++ src/com/google/gwt/inject/rebind/ErrorManager.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.inject.rebind;
+
+import com.google.gwt.core.ext.TreeLogger;
+import com.google.gwt.core.ext.UnableToCompleteException;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * Manages errors that occur during the binding process.  Keeps track of whether
+ * we've found an error so we can eventually throw an
+ * {@link UnableToCompleteException}. We do this instead of throwing
+ * immediately so that we can find more than one error per compilation cycle.
+ */
+@Singleton
+public class ErrorManager {
+  private boolean foundError = false;
+  private final TreeLogger logger;
+
+  @Inject
+  public ErrorManager(TreeLogger logger) {
+    this.logger = logger;
+  }
+
+  public void logError(String message) {
+    logError(message, null);
+  }
+
+  public  void logError(String message, Throwable t) {
+    logger.log(TreeLogger.ERROR, message, t);
+    foundError = true;
+  }
+
+  public void checkForError() throws UnableToCompleteException {
+    if (foundError) {
+      throw new UnableToCompleteException();
+    }
+  }
+}
